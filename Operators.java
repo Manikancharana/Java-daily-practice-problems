c03f09495fdf7a2cc4c9a0c58d7e7fcb6cc8e39c
Kancharana manikanta <kancharanamanikanta26@email.com>
2025-12-16 05:51:24 +0530

    casting, operators

---
+++ Operators.java
@@ -0,0 +1,8 @@
+public class Operators {
+    public static void main(String[] args) {
+        //arthametic operators +,-,*,/,%
+        int x=25;
+        int y=40;
+        System.out.println(x+y);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        // System.out.print("Hello World!");
+        //implicit type casting
+        int a=5;
+        double aa=a;
+        System.out.println(aa);
+        //Explicit casting
+        double x=24.54;
+        int y=(int)x;
+       System.out.println(y);
+        
+    }
+}
