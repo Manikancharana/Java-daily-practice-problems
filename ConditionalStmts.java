939ab8dfe9c9bc15ef752bd6aedea6b599c9816c
Kancharana manikanta <kancharanamanikanta26@email.com>
2025-12-17 17:37:08 +0530

    Oops

--- ConditionalStmts.java
+++ ConditionalStmts.java
@@ -1,15 +1,67 @@
 import java.util.Scanner;
 public class ConditionalStmts {
     public static void main(String[] args) {
-     Scanner sc =new Scanner(System.in);
-     System.out.println("Enter your Bill amount.");
-     int amount=sc.nextInt();
-     if(amount>1000){
-        System.out.println("Offer valid!....");
-     }
-     else{
-        System.out.println("Offer not valid!");
-     }
-  sc.close();
+   //   Scanner sc =new Scanner(System.in);
+   //   System.out.println("Enter your Bill amount.");
+   //   int amount=sc.nextInt();
+   //   if(amount>1000){
+   //      System.out.println("Offer valid!....");
+   //   }
+   //   else{
+   //      System.out.println("Offer not valid!");
+   //   }
+
+//    if (amount < 1000) {
+//     System.out.println("Offer not valid!");
+// }
+// else if (amount >= 1000 && amount < 1500) {
+//     System.out.println("50% Discount");
+// }
+// else if (amount >= 1500) {
+//     System.out.println("80% Discount");
+// }
+// else {
+//     System.out.println("Purchase anything you want in the store");
+// }
+ 
+//ternary operator variable=condition ? value_if_true : value_if_false 
+
+// int count=15;
+//     String num=(count>10)? "Section-A" : "Section-B";
+//     System.out.println(num);
+
+//switch case 
+   // switch (args) {
+   //    case value1: statemts
+   //       break;
+   //       case value2: statemts
+   //       break;
+   
+   //    default:statemts
+   //       break;
+   // }
+
+// loops
+//    for(int i=1;i<5;i++){
+//       for(int j=0;j<i;j++){
+//       System.out.print("*");
+//    }
+//     System.out.println();
+// }
+
+// while (condition) { // until the condition true it will not exicute 
+//    statments....
+// }
+
+//do while 1st exicute once and ofter checks the condition true or false
+// int i=4;
+// do{
+//    System.out.println("Positive numbers");
+// }while(i>0);
+
+
+
+
+//   sc.close();
     }
 }
---
+++ Opps concepts/Student.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Student{
+    public static void main(String[] args) {
+        
+   
+    int[] rlno =new int[5];
+    String[] name= new String[5];
+    float[] marks = new float[5];
+
+    Std[] students= new Std[5];
+    System.out.println(Arrays.toString(students));
+
+   
+    }
+     }
+
+ class Std{
+        int rlno;
+        String name;
+        float marks;
+ }
